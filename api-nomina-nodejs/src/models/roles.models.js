4c3511e919d6d635c1e746a8dac18c266f19c545
Luis Astorga <luis.astorgaesquivel@gmail.com>
2026-04-03 16:45:16 -0600

    Add files via upload

---
+++ api-nomina-nodejs/src/models/roles.models.js
@@ -0,0 +1,32 @@
+const { mysqlPool } = require('../config/mysql');
+
+class Roles {
+    //Obtener todos los roles
+    static async obtenerTodos() {
+        const [rows] = await mysqlPool.query(
+            "SELECT IdRol_PK,TipoRol FROM Roles");
+
+        return rows;
+    }
+
+    //Insertar rol 
+    static async CrearRol() {
+        const {TipoRol} = data;
+        const [result] = await mysqlPool.query(
+            "INSERT INTO Roles (TipoRol) VALUES (?)",
+            [TipoRol]
+        );
+        return { IdRol_PK: result.insertId, TipoRol };
+    }
+
+    //Elimiar rol
+    static async EliminarRol(id) {
+        const [result]= await mysqlPool.query(
+            "DELETE FORM Roles WHERE IdRol_PK = ?",
+            [id]
+        );
+        return result.affectedRows > 0;
+    }
+}
+
+module.exports = Roles;
