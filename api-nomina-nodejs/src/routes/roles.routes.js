3463bad7718b2c554e3a52a8888d19a355ac4518
222310645 <222310645@itslerdo.edu.mx>
2026-04-19 19:01:33 -0600

    Delete api-nomina-nodejs directory

--- api-nomina-nodejs/src/routes/roles.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RolesController = require('../controllers/roles.controller');
-
-router.get('/', RolesController.obtenerRoles);
-router.post('/', RolesController.InsertarRol);
-router.delete('/:id', RolesController.EliminaRol);
-module.exports = router;
